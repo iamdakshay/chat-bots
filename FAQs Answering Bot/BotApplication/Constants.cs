700d556af74320587e2fdfb2bc14b058972ead42
Akshay Deshmukh <akshay.deshmukh@nitorinfotech.com>
2017-07-06 20:03:12 +0530

    Chat bot with LUIS

---
+++ FAQs Answering Bot/BotApplication/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BotApplication
+{
+    public class Constants
+    {
+        public const string LUIS_EMPLOYEE_HELPER_APP_ID = "";
+        public const string LUIS_SUBSCRIPTION_KEY = "";
+    }
+}
