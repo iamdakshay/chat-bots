faed36bedd4fddc7eb87a4566194b7849accfee4
Akshay Deshmukh <akshay.deshmukh@nitorinfotech.com>
2017-08-20 15:57:06 +0530

    FormFlow example is added

---
+++ Bots-Using Microsoft Bot Framework/BotApplication/Controllers/PartyRegistrationController.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.FormFlow;
+using BotApplication.Forms;
+
+namespace BotApplication
+{
+    //[BotAuthentication]
+    public class PartyRegistrationController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, MakeForm);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        internal static IDialog<PartyRegistrationForm> MakeForm()
+        {
+            return Chain.From(() => FormDialog.FromForm(PartyRegistrationForm.BuildForm))
+                .Do(async (context, order) =>
+                {
+                    try
+                    {
+                        var completed = await order;
+                        await context.PostAsync("You are registerd. Have a happy time with us.");
+                    }
+                    catch (FormCanceledException<PartyRegistrationForm> e)
+                    {
+                        string reply = null == e.InnerException ?
+                        $"Hey, you quit the registration. Dont miss out the party!" :
+                        "Sorry, Could not register you. Please try again.";
+                        await context.PostAsync(reply);
+                    }
+                });
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Bots-Using Microsoft Bot Framework/BotApplication/Forms/PartyRegistrationForm.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using System;
+using System.Collections.Generic;
+using static BotApplication.Constants;
+
+namespace BotApplication.Forms
+{
+    [Serializable]
+    public class PartyRegistrationForm
+    {
+        [Prompt("May I know your good name?")]
+        public string Name; // type: String
+
+        [Optional]
+        [Template(TemplateUsage.EnumSelectOne, "Please select gender, if you want else you can skip. {||}", ChoiceStyle = ChoiceStyleOptions.Buttons)]
+        public GenderOpts? Gender; // type: Enumeration
+
+        [Prompt("What will be your arrival time?")]
+        public DateTime ArrivalTime; // type: DateTime
+
+        [Numeric(1, 6)]
+        [Prompt("How many guests are accompanying you?<br>If more than 3, you will get complementory drink ! :)")]
+        public Int16 TotalAttendees; // type: Integral
+
+        [Prompt("Which type of cuisines you would like to have? {||}")]
+        public List<CuisinesOpts> CuisinesPreferences; // type: List of enumerations
+
+        [Template(TemplateUsage.EnumSelectOne, "Which complementory drink you would like to have? {||}", ChoiceStyle = ChoiceStyleOptions.Carousel)]
+        public ComplementoryDrinkOpts? ComplementoryDrink; // type: Enumeration
+
+        public static IForm<PartyRegistrationForm> BuildForm()
+        {
+            return new FormBuilder<PartyRegistrationForm>()
+                    .Message("Hey, Welcome to the registartion for New Year's Eve party !")
+
+                    .Field(nameof(Name))
+                    .Field(nameof(Gender))
+                    .Field(nameof(ArrivalTime))
+                    .Field(nameof(TotalAttendees))
+                    .Field(nameof(CuisinesPreferences))
+                    .Field(new FieldReflector<PartyRegistrationForm>(nameof(ComplementoryDrink))
+                        .SetType(null)
+                        .SetActive(state => state.TotalAttendees > 3)
+                        .SetDefine(async (state, field) =>
+                        {
+                            field
+                            .AddDescription(ComplementoryDrinkOpts.Beer, Convert.ToString(ComplementoryDrinkOpts.Beer),
+                            "<<image path>>")
+                            .AddTerms(ComplementoryDrinkOpts.Beer, Convert.ToString(ComplementoryDrinkOpts.Beer))
+
+                            .AddDescription(ComplementoryDrinkOpts.Scotch, Convert.ToString(ComplementoryDrinkOpts.Scotch),
+                            "<<image path>>")
+                            .AddTerms(ComplementoryDrinkOpts.Scotch, Convert.ToString(ComplementoryDrinkOpts.Scotch))
+
+                            .AddDescription(ComplementoryDrinkOpts.Mojito, Convert.ToString(ComplementoryDrinkOpts.Mojito),
+                            "<<image path>>")
+                            .AddTerms(ComplementoryDrinkOpts.Mojito, Convert.ToString(ComplementoryDrinkOpts.Mojito));
+
+                            return true;
+                        }))
+                    .Confirm(async (state) =>
+                    {
+                        return new PromptAttribute("Hi {Name}, Please review your selection. No. of guests: {TotalAttendees}, Cuisines: {CuisinesPreferences}. Do you want to continue? {||}");
+                    })
+                    .Build();
+        }
+    }
+}
